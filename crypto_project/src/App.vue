94537aee09d6c2b4d5e91c5fe49bf6bae8515ccd
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-04-06 20:28:45 +0200

    set-up project

---
+++ crypto_project/src/App.vue
@@ -0,0 +1,17 @@
+<script >
+import componentCrypto from './components/ComponentCrypto.vue';
+
+export default {
+  components: {
+    componentCrypto
+  }
+}
+</script>
+
+<template>
+  <componentCrypto />
+</template>
+
+<style scoped>
+
+</style>
---
+++ crypto_project/src/components/ComponentCrypto.vue
@@ -0,0 +1,25 @@
+<script>
+import CryptoJS from "crypto-js"
+export default {
+  name: "componentCrypto",
+  mounted() {
+    const encryptedText = CryptoJS.AES.encrypt("Hi There!", "Secret Passphrase").toString();
+    console.log(encryptedText);
+
+    var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
+
+    var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+
+    console.log(encrypted);
+    console.log(decrypted);
+  },
+};
+</script>
+
+<template></template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
