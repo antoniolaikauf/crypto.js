ede970e4c08ba75cacf08e2e412b923e007513c8
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-04-15 18:10:45 +0200

    add text in desprocesso

--- crypto_project/src/components/ComponentDesProcesso.vue
+++ crypto_project/src/components/ComponentDesProcesso.vue
@@ -13,5 +13,13 @@ export default {
     CryptoJS.AES.encrypt(Message, key); var decrypted = CryptoJS.AES.decrypt(encrypted, key); Questo è il procedimento di base per cifrare e decifrare
     un messaggio.
   </p>
+  <!-- teoria  -->
+  <p>
+    È molto importante sottolineare che, per crittografare del testo, si dovrebbero utilizzare degli standard come CCM, GCM ed EAX. Questi standard
+    sono sviluppati dal National Institute of Standards and Technology (NIST) e rappresentano delle modalità che garantiscono sia la confidenzialità
+    sia l'integrità dei dati. Pertanto, sono modelli che resistono sia ad attacchi all'integrità (in cui il messaggio potrebbe essere stato
+    modificato) sia a violazioni della confidenzialità, assicurando che solo le parti autorizzate possano accedere al contenuto di un dato o messaggio
+    quindi soggetti ad attacchi CCA (Chosen Ciphertext Attack) e CPA (Chosen Plaintext Attack).
+  </p>
 </template>
 <style></style>
