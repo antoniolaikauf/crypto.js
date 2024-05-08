dd57a838b1b57428ce2f73687f7506728af8cf4e
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-05-08 17:41:50 +0200

    aggiunto : aggiunto conversione key

--- crypto_project/src/components/ComponentCryptoEsempio.vue
+++ crypto_project/src/components/ComponentCryptoEsempio.vue
@@ -3,13 +3,28 @@ import CryptoJS from "crypto-js";
 export default {
   name: "EsCrypto_js",
   data() {
-    return {};
+    return {
+      key: "",
+      key_hex: "",
+    };
   },
-  methods: {},
+  methods: {
+    ciphertext() {
+      for (const caracter of this.key) {
+        this.key_hex += caracter.charCodeAt(0).toString(16);
+      }
+    },
+  },
+  mounted() {},
 };
 </script>
 <template>
-  <section>ESEMPIO.</section>
+  <section>
+    ESEMPIO.
+    <div>Inserisci una key <input type="text" v-model="key" /></div>
+
+    <button @click="ciphertext">coonverti</button>
+  </section>
 </template>
 <style scoped lang="scss">
 @use "./../style/partials/_partials.scss" as *;
