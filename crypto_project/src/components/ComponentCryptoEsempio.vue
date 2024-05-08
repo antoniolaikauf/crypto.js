8e74508629bd74d8cbefedff2bdb543e1eda8cb4
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-05-08 18:54:27 +0200

    fix: sistemato lunghezza key

--- crypto_project/src/components/ComponentCryptoEsempio.vue
+++ crypto_project/src/components/ComponentCryptoEsempio.vue
@@ -13,15 +13,14 @@ export default {
   },
   methods: {
     ciphertext() {
-      let salt = CryptoJS.lib.WordArray.random(128 / 8);
-      this.key_hex = CryptoJS.PBKDF2(this.key, salt, {
+
+      let salt = CryptoJS.lib.WordArray.random(128 / 8); //generazione sale per dare randomicità 
+      this.key_hex = CryptoJS.PBKDF2(this.key, salt, { // generazione key di lunghezza 128 bits si usa un PBKDF e non un HKDF 
         keySize: 128 / 8,
       }).toString();
 
-      console.log(salt);
-      console.log(this.key_hex);
-      this.text_cipher = CryptoJS.AES.encrypt(this.message, this.key_hex, {
-        // si cifra il testo con cifrario CTR contatore randomizzato
+      this.text_cipher = CryptoJS.AES.encrypt(this.message, this.key_hex, { // cifratura a blocchi usando un AES
+        // archittettura CTR contatore randomizzato
         mode: CryptoJS.mode.CTR,
       });
       console.log(this.text_cipher);
