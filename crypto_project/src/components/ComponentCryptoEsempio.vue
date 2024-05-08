6924858b9aad93a6e3de503b8253a2c72f0d8944
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-05-08 18:06:03 +0200

    aggiunto: messo testo cifrato per cifrare i messaggi

--- crypto_project/src/components/ComponentCryptoEsempio.vue
+++ crypto_project/src/components/ComponentCryptoEsempio.vue
@@ -7,15 +7,21 @@ export default {
       key: "",
       key_hex: "",
       key_crypto_js: "",
+      message: "",
+      text_cipher: "",
     };
   },
   methods: {
     ciphertext() {
       for (const caracter of this.key) {
-        this.key_hex += caracter.charCodeAt(0).toString(16);
+        this.key_hex += caracter.charCodeAt(0).toString(16); // si trasforma il testo in esadecimale
       }
-      this.key_crypto_js = CryptoJS.enc.Hex.parse(this.key_hex);
-      console.log(this.key_crypto_js);
+      this.key_crypto_js = CryptoJS.enc.Hex.parse(this.key_hex); // si adatta la key esadecimale in modo tale che la libreria mossa usarlo
+      this.text_cipher = CryptoJS.AES.encrypt(this.message, this.key_crypto_js.toString(), {
+        // si cifra il testo
+        mode: CryptoJS.mode.CTR,
+      });
+      console.log(this.text_cipher);
     },
   },
   mounted() {},
@@ -25,8 +31,11 @@ export default {
   <section>
     ESEMPIO.
     <div>Inserisci una key <input type="text" v-model="key" /></div>
+    <div>Inserisci il messaggio <input type="text" v-model="message" /></div>
 
     <button @click="ciphertext">coonverti</button>
+
+    <div>il testo cifrato è il seguente usando un cifrario a blocchi CTR{{ text_cipher.toString(16) }}</div>
   </section>
 </template>
 <style scoped lang="scss">
