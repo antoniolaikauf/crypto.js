6d8751b658d7789712f25a3424d7b6977547b51a
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-05-12 16:30:58 +0200

    aggiunto mac encryption

--- crypto_project/src/components/ComponentCryptoEsempio.vue
+++ crypto_project/src/components/ComponentCryptoEsempio.vue
@@ -9,24 +9,35 @@ export default {
 
       message: "",
       text_cipher: "",
+      pachetto: "",
     };
   },
   methods: {
     ciphertext() {
       // var iv = CryptoJS.enc.Hex.parse("101112131415161718191a1b1c1d1e1f"); IV FISSO COSA SBAGLIATA PERCHè IV DEVE ESSERE RANDOMICO E OGNI VOLTA CHE SI CIFRA UN MESSAGGIO NON DEVE ESSERE MAI STATO USATO QUEL IV
-      let salt = CryptoJS.lib.WordArray.random(128 / 32); //generazione sale per dare randomicità
-      this.key_hex = CryptoJS.PBKDF2(this.key, salt, {
+      let saltEncyption = CryptoJS.lib.WordArray.random(128 / 32); //generazione sale per dare randomicità
+      this.key_hex_encyption = CryptoJS.PBKDF2(this.key, saltEncyption, {
         // generazione key di lunghezza 128 bits si usa un PBKDF e non un HKDF
         keySize: 128 / 32,
       }).toString();
 
-      this.text_cipher = CryptoJS.AES.encrypt(this.message, this.key_hex, {
+      this.text_cipher = CryptoJS.AES.encrypt(this.message, this.key_hex_encyption, {
         // cifratura a blocchi usando un AES
         // archittettura CTR contatore randomizzato
         mode: CryptoJS.mode.CTR,
         iv: iv,
       });
       console.log(this.text_cipher);
+      // procedura mac la key deve essere diversa da quella dell'encyption
+      let saltMac = CryptoJS.lib.WordArray.random(128 / 32);
+      let keyMac = CryptoJS.PBKDF2("frase", saltMac, {
+        keySize: 128 / 32,
+      }).toString();
+      console.log(keyMac);
+
+      // concatenazione
+
+    
     },
   },
   mounted() {},
